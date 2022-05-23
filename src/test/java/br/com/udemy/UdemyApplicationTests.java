c9fa9aa2f535f06e092166319faa58831750c6c1
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-22 21:11:20 -0300

    fix: remove code smells

--- src/test/java/br/com/udemy/UdemyApplicationTests.java
+++ src/test/java/br/com/udemy/UdemyApplicationTests.java
@@ -1,7 +1,13 @@
 package br.com.udemy;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UdemyApplicationTests {
+
+    @Test
+    void main() {
+        UdemyApplication.main(new String[] {});
+    }
 }
